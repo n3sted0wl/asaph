77adb7a727896d1c4347d980cbd4464adbb34334
Paul Antonio <pantonio@preferred-mfg.com>
2021-06-19 13:54:38 -0400

    added more conditions to song title persistence test

--- Asaph/Tests.BusinessRules.General/SongTitles/Persistence.cs
+++ Asaph/Tests.BusinessRules.General/SongTitles/Persistence.cs
@@ -17,18 +17,39 @@ namespace Asaph.Tests.BusinessRules.General.SongTitles {
         [Fact]
         public void SongTitlesCanBeWrittenAndRead() {
             SongTitlesFactory songTitlesFactory = AsaphInjectionManager().BusinessRulesInjector().SongTitlesFactory();
-            SongTitle firstSongTitle = songTitlesFactory.Builder().Build("First Song Title");
+            SongTitle firstSongTitle  = songTitlesFactory.Builder().Build("First Song Title");
             SongTitle secondSongTitle = songTitlesFactory.Builder().Build("Second Song Title");
+            SongTitle thirdSongTitle  = songTitlesFactory.Builder().Build("Third Song Title");
+            SongTitle fourthSongTitle = songTitlesFactory.Builder().Build("Fourth Song Title");
+            SongTitle fifthSongTitle  = songTitlesFactory.Builder().Build("Fifth Song Title");
+            SongTitle sixthSongTitle  = songTitlesFactory.Builder().Build("Sixth Song Title");
             Assert.NotEqual(firstSongTitle.RecordId, secondSongTitle.RecordId);
 
-            List<Task<AsaphOperationResult<SongTitle>>> saveResults = new() {
+            List<Task<AsaphOperationResult<SongTitle>>> individualSaveResults = new() {
                 songTitlesFactory.Writer().Save(firstSongTitle),
                 songTitlesFactory.Writer().Save(secondSongTitle)
             };
 
-            List<Task<AsaphOperationResult<SongTitle>>> failedSaveResults = 
-                saveResults.Where(task => task.Result.HasFailureStatus).ToList();
-            Assert.Empty(failedSaveResults);
+            List<Task<AsaphOperationResult<IEnumerable<SongTitle>>>> batchSaveResults = new() {
+                songTitlesFactory.Writer().Save(new List<SongTitle> { 
+                    thirdSongTitle, 
+                    fourthSongTitle
+                }),
+                songTitlesFactory.Writer().Save(new List<SongTitle> { 
+                    fifthSongTitle, 
+                    sixthSongTitle
+                }),
+            };
+
+            List<Task<AsaphOperationResult<SongTitle>>> failedIndividualSaveResults = 
+                individualSaveResults.Where(task => task.Result.HasFailureStatus).ToList();
+            Assert.Equal(2, individualSaveResults.Count);
+            Assert.Empty(failedIndividualSaveResults);
+
+            List<Task<AsaphOperationResult<IEnumerable<SongTitle>>>> failedBatchSaveResults =
+                batchSaveResults.Where(task => task.Result.HasFailureStatus).ToList();
+            Assert.Equal(2, batchSaveResults.Count);
+            Assert.Empty(failedBatchSaveResults);
         }
     }
 }
